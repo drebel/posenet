2b64d324a42d353c9347b5c8ca4e304675dde0c9
drew <drewbeland@gmail.com>
2024-04-04 12:27:42 -0700

    added dependencies and webcam component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,51 @@
+import React, { useRef } from 'react'
+import * as poseDetection from '@tensorflow-models/pose-detection'
+import * as tf from '@tensorflow/tfjs-core'
+import '@tensorflow/tfjs-backend-wasm'
+import Webcam from 'react-webcam'
 
 import './App.css'
 
 function App() {
+
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
  
   return (
-    <>
-      <h1>hello</h1>
-    </>
-  )
+    <div className="App">
+      <header className="App-header">
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+      </header>
+    </div>
+  );
 }
 
 export default App
