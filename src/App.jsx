fa04a3a3ca426fdb99448a1f132d0da15c651914
drew <drewbeland@gmail.com>
2024-04-04 13:05:28 -0700

    console log poses

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,41 @@ function App() {
 
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
- 
+
+  //load movenet model
+  const runMovenet = async () => {
+    const detectorConfig = {modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING};
+    const detector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, detectorConfig);
+    detect(detector)
+  }
+
+  const detect = async (detector) => {
+    if (
+      // make sure webcam is receiving video
+      typeof webcamRef.current !== 'undefined' &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ){
+      // get video properties
+      const video = webcamRef.current.video
+      const videoWidth = webcamRef.current.video.videoWidth
+      const videoHeight = webcamRef.current.video.videoHeight
+
+      // set video width
+      webcamRef.current.video.width = videoWidth
+      webcamRef.current.video.height = videoHeight
+
+      // make detections
+      const poses = await detector.estimatePoses(video)
+      console.log(poses)
+    }
+  }
+
+  runMovenet()
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div >
+      <header >
         <Webcam
           ref={webcamRef}
           style={{
